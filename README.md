c04ecb9d335d365c297308ebb71ada2ccafab179
Josep Rueda <rueda_999@hotmail.com>
2024-12-19 13:35:28 +0100

    simulation navigation added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
+# galax_navigation
 
-
-ros2 run nav2_map_server map_saver_cli -f my_map3# galax_navigation
+ros2 run nav2_map_server map_saver_cli -f my_sim_map
--- launch/navigation_launch.py
+++ launch/navigation_launch.py
@@ -56,7 +56,7 @@ def generate_launch_description():
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static'),
-                  ('/cmd_vel','differential_controller/cmd_vel_unstamped'),]
+                  ('/cmd_vel','cmd_vel/raw'),]
 
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
@@ -181,7 +181,7 @@ def generate_launch_description():
                 parameters=[configured_params],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings +
-                        [('/cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'differential_controller/cmd_vel_unstamped')]),
+                        [('cmd_vel', 'differential_controller/cmd_vel_unstamped'), ('cmd_vel_smoothed', '/differential_controller/cmd_vel_unstamped')]),
             Node(
                 package='nav2_lifecycle_manager',
                 executable='lifecycle_manager',
@@ -240,7 +240,7 @@ def generate_launch_description():
                 name='velocity_smoother',
                 parameters=[configured_params],
                 remappings=remappings +
-                           [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
+                           [('cmd_vel', '/differential_controller/cmd_vel_unstamped'), ('cmd_vel_smoothed', '/differential_controller/cmd_vel_unstamped')]),
             ComposableNode(
                 package='nav2_lifecycle_manager',
                 plugin='nav2_lifecycle_manager::LifecycleManager',
